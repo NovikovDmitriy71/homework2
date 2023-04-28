bf2da5c308c56716100edb377a5da5375870cf28
dmitrij <galactik71@mail.ru>
2023-04-29 01:21:54 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// // 1: 293 -> 9, 128 -> 2
+// Console.WriteLine("input namber ");
+// int a=Convert.ToInt32(Console.ReadLine());
+// int des=((a % 100) / 10);
+// Console.WriteLine($"{a} -> {des}");
+
+// // 1: 293 -> 3, 12 -> третий цифры нет
+
+
+// Console.WriteLine("Введите день недели");
+// int num= Convert.ToInt32(Console.ReadLine());
+// if (num >= 1 && num <=7 )
+// {
+//     if (num == 6 || num == 7)
+//     Console.WriteLine($" {num}, Выходной день");
+//     else 
+//     Console.WriteLine($" {num}, Рабочий дкнь");
+// }
+
+
+
